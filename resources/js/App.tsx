e108a1b5a696d14e7ed67adc4975e0f3133a01d2
ErmLuke <81567252+ErmLuke@users.noreply.github.com>
2025-04-10 16:19:59 +0100

    Feature: Added customer , terminal and catch all route to app.tsx

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -1,26 +1,31 @@
 import {
     BrowserRouter,
     Routes,
-    Route,
+    Route, Navigate
 } from 'react-router-dom';
 import Login from './pages/Login';
-import Home from './pages/Home';
+import Customers from './pages/Customers';
 import { GuestRoute, PrivateRoute } from '@/utils/AuthRouter';
 import MainLayout from './layouts/MainLayout';
+import TerminalDashboardWrapper from '@/pages/terminal-dashboard/TerminalDashboardWrapper';
+import Home from './pages/Home';
 
 const AppRouter = () => {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<GuestRoute />}>
-                    <Route path="/login" element={<Login />} />
-                </Route>
+                    <Route element={<GuestRoute />}>
+                        <Route path="/login" element={<Login />} />
+                    </Route>
                 <Route element={<PrivateRoute />}>
                     <Route element={<MainLayout/>}>
-                    <Route path="/home" element={<Home />} />
+                        <Route path="/home" element={<Home /> } />
+                        <Route path="/terminals" element={<Customers />} />
+                        <Route path="/terminals/:terminalId" element={<TerminalDashboardWrapper />} />
                     </Route>
                 </Route>
+                <Route path="/*" element={<Navigate to="/login" replace />}/>
             </Routes>
         </BrowserRouter>
     );
